353a78a5bd59728d955215333dc2648e5c62d845
Jonathan Harder <jonharder6@gmail.com>
2018-04-21 23:04:39 -0500

    add menu items to drawer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
 import DatePicker from 'material-ui/DatePicker';
+import Drawer from 'material-ui/Drawer';
 
 
-const Header = ({title}) =>
-    <AppBar title={title} iconClassNameRight="muidocs-icon-navigation-expand-more" />
+const Header = (props) =>
+    <AppBar title={props.title}
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonClick={props.onLeftButtonClick} />
+
+
+const SideBar = (props) =>
+    <Drawer open={props.open}
+            docked={false}
+            onRequestChange={props.close}>
+        <Menu>
+            <MenuItem primaryText="One" />
+            <MenuItem primaryText="Two" />
+            <MenuItem primaryText="Three" />
+        </Menu>
+    </Drawer>
 
 
 class App extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.toggleDrawer = this.toggleDrawer.bind(this);
+
+        this.state = {open: false};
+    }
+
+
+    toggleDrawer() {
+        this.setState({open: !this.state.open});
+    }
+
+    closeDrawer() {
+        this.setState({open: false});
+    }
+
+
     render() {
         const title = "Jogger";
         
         return (
             <MuiThemeProvider className="App">
                 <div>
-                    <Header title={title} />
+                    <SideBar close={() => this.setState({open: false})}
+                             open={this.state.open} />
+                    <Header onLeftButtonClick={this.toggleDrawer} title={title} />
                     <div>
-                        <RaisedButton label="Foo" />
+                        <RaisedButton onClick={this.toggleDrawer} label="Foo" />
                         <DatePicker hintText="What Date did you run on?" />
                     </div>
                 </div>
